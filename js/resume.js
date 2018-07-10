045e39f8f90b66b4c2f2aefe2c789e088043c22e
shineGG <38377975+shineGG@users.noreply.github.com>
2018-07-10 15:06:39 +0800

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -14,7 +14,7 @@
 		seintro:"喜欢互联网、前端技术，自学能力较强，好奇心较强但会服从工作安排完成工作。在学习前端过程中简单的编写了一个个人主页，网址为：http://shinegg.github.io，虽然目前此个人项目较简单，且也不是我在学习前端过程中所有做过的练习，但是一定程度上练习了我个人在前端上的基本知识，以及展现了我对前端的热爱及想做出更多东西的信心。我会继续完善此个人网页项目。",
 		edu1:"2014.06-2018.06&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;江苏科技大学&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;船舶与海洋工程（本科）",
 		edu2:"主修课程：C++源程序设计；大三至大四期间主要自学HMTL、CSS、JavaScript、 jQuery、Ajax、 Angular、Bootstrap等课程。",
-		exp1:"在校期间学习WEB前端技术，熟悉HTML 、CSS、 JavaScript、 Jquery、 Ajax、 Angular、bootstrap等技术；",
+		exp1:"在校期间学习WEB前端技术，熟悉HTML 、CSS、 JavaScript、 Jquery、 Ajax、 Angular、bootstrap、vue、echarts、微信小程序开发、mpvue等技术；",
 		exp2:"练习模仿编写某网络交易平台首页及详情页面、练习编写2048网页游戏；",
 		exp3:"学习部分PHP及MySql语言，会使用XAMPP软件结合Ajax、服务器PHP页面及数据库MySql查询语句测试网页；",
 		exp4:"练习使用Bootstrap及Angular编写订餐响应式单页应用。对less有一定了解。",
